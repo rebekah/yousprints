307d14e286adbd32ca0c8bb4e63747b70ebeb43d
Rebekah Waterbury <rebekah@rebekah-waterburys-macbook-air-681.local>
2013-05-27 17:54:07 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yousprints::Application.config.secret_token = '6a900b1c9467a9a4ac3df2a8b6df1089792d41160f3ec486b6b9607dd07f31985a8a0064499e902016012294fa5653d7b8479032840f5eb37e75bbcb16f32f5b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Yousprints::Application.config.session_store :cookie_store, key: '_yousprints_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Yousprints::Application.config.session_store :active_record_store
